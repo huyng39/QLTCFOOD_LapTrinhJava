9abfcdcd8c9982d10259a864f9475d2f73d121e8
HUY DOREMON 7012 <huyng39@yahoo.com>
2023-06-27 11:31:36 +0700

    first commit

---
+++ src/DAO/ConnectSQL.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ConnectSQL {
+    public static String databaseName = "DoAnCoSoQLTCF";
+    public static Connection getConnection() {
+    String url = "net.sourceforge.jtds.jdbc.Driver";
+        try {
+            Class.forName(url);
+            String dbUrl = "jdbc:jtds:sqlserver://localhost:1433/DoAnCoSoQLTCF;instance=HUY_DOREMON_70";
+            //String dbUrl = "jdbc:jtds:sqlserver://localhost:1433/DoAnCoSoQLTCF;instance=SQLEXPRESS";
+            return DriverManager.getConnection(dbUrl,"sa","huysql12308");
+            //return DriverManager.getConnection(dbUrl,"sa","sa");
+//          return DriverManager.getConnection(dbUrl,user, pass);
+        } catch (Exception ex) {
+            Logger.getLogger(ConnectSQL.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        ConnectSQL.getConnection();
+    }
+}
